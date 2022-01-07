443e1de511e54fbd8f8f09d6171d45edbb037109
SERG <serdgant66@mail.ru>
2022-01-07 15:15:38 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket = 2600;
+        int m = 20;
+        int bonus = ticket / m;
+        System.out.println("миль получено" + bonus);
+    }
+}
